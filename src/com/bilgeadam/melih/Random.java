6888334c7ae5433b6fb8bf0faa7f4f5be51722c5
melihdumanli <melih.dumanli@bilgeadamboost.com>
2021-09-28 10:51:01 +0300

    Single Array, Multi Array

---
+++ src/com/bilgeadam/melih/Random.java
@@ -0,0 +1,16 @@
+package com.bilgeadam.melih;
+
+public class Random {
+    public static void main(String[] args) {
+
+        // Math Random
+        double random = Math.floor(Math.random()*5+1);
+        System.out.println(random);
+
+        //Class Random
+        // java.util
+        java.util.Random randomClass = new java.util.Random();
+        int random2 = randomClass.nextInt(9) +1;
+        System.out.println(random2);
+    }
+}
---
+++ src/com/bilgeadam/melih/velisgame.java
@@ -0,0 +1,30 @@
+package com.bilgeadam.melih;
+
+import java.util.Scanner;
+
+public class velisgame {
+    public static void main(String[] args) {
+
+        BAUtils.header("WELCOME TO HELL");
+        boolean[] spints = new boolean[1000];
+        for (int i=0; i< spints.length; i++) {
+            spints[i] = false;
+        }
+
+        for (int tour =1; tour<= spints.length; tour++) {
+            for (int i=0; i< spints.length;) {
+                spints[i] = !spints[i];
+                i = i + tour;
+            }
+        }
+        int count = 0;
+        for (int i=0; i< spints.length;i++){
+            if (spints[i]) {
+                System.out.println(i + " ");
+                count++;
+            }
+        }
+        System.out.println("\n" + count);
+
+    }
+}
